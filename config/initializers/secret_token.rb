2e62f800fadf330e93abe26e4d6319df537aa5e2
matteogiacalone <m.giacalone@pigeko.com>
2013-07-17 23:28:52 +0200

    First Lect

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondProg::Application.config.secret_token = '4556457676cf28d0a28677034fa82e0c41f587ef823d62d707b457b990e54ecfc0fe9e05045be93dd2758360c0c92a68f049de90b66e0976a6e7f315734e1b2f'
